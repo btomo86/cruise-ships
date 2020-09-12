93c24f4b333ab66f93c1debff6a0b941160d3bcd
Ben Thompson <btomo86@gmail.com>
2020-09-12 14:06:18 +0100

    first attempt at making it dry

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -3,7 +3,21 @@ const Port = require('../src/Port.js')
 const Itinerary = require('../src/Itinerary');
 
 describe('Ship', () => {
-    test('can be instantiated', () => {
+  describe('with ports and an itinerary', () => {
+    let ship;
+    let dover;
+    let calais;
+    let itinerary;
+  
+    beforeEach(() => {
+      dover = new Port('Dover');
+      calais = new Port('Calais');
+      itinerary = new Itinerary([dover, calais]);
+      ship = new Ship(itinerary)
+    });
+  
+    
+  });
       const port = new Port('Dover');
       const itinerary = new Itinerary([port]);
       const ship = new Ship(itinerary);
@@ -16,10 +30,6 @@ describe('Ship', () => {
       expect(ship.currentPort).toBe(port);
     });
     test('it can set sail', () => {
-      const dover = new Port('Dover');
-      const calais = new Port('Calais');
-      const itinerary = new Itinerary([dover, calais]);
-      const ship = new Ship(itinerary);
       ship.setSail();
       expect(ship.currentPort).toBeFalsy();
       expect(dover.ships).not.toContain(ship);
@@ -28,20 +38,12 @@ describe('Ship', () => {
       
     
     test('Can dock at different port', () => {
-      const dover = new Port('Dover');
-      const calais = new Port('Calais');
-      const itinerary = new Itinerary([dover, calais])
-      const ship = new Ship(itinerary);
       ship.setSail();
       ship.dock();
       expect(ship.currentPort).toBe(calais);
       expect(calais.ships).toContain(ship);
     });
     test('can\'t sail further than its ininerary', () => {
-      const dover = new Port('Dover');
-      const calais = new Port('Calais');
-      const itinerary = new Itinerary([dover, calais]);
-      const ship = new Ship(itinerary);
       ship.setSail();
       ship.dock();
       expect(()=> ship.setSail()).toThrowError('End of itinerary reached');
